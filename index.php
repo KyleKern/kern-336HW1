b3442963a238d47adadae500a97dfd042e1656e7
Kyle Kern <kykern@csumb.edu>
2017-09-03 19:45:47 +0000

    hello

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <header>
+            <h1>Programming Facts</h1>
+        </header>
+        <nav> 
+            <hr width="50%"/>
+            <a href="index.php" color= white><strong> Home </strong></a>
+            <a href="Cplus.html"> C++  </a>
+            <a href="java.html"> Java </a>
+             <a href="python.html"> Python </a>
+            <a href="contact.html">Contact</a>
+        </nav>
+        <br /><br />
+        <main>
+            <figure id="me">
+                <img src="coding-types.jpg" width="640" height="360" alt="Logo for C++" />
+            </figure>
+            
+            <div id="welcomeText">
+                Hello! <br/>
+                <p>C++ is a very useful and wide spread programming language. Some ways it is used is in game programming with directX, and you can even use it for simpler programming</p>
+                <p> on the pages ill go into more depth about what kinds of programming you can do with C++</p>
+                <br /><br />
+            </div>
+            <div style="clear: both;">
+        </main>
+        <footer>
+            <hr>
+            Internet programming 2017 Kern <br />
+            <strong>
+                Disclamer: </strong> The information in this webpage is fictitous. <br />
+            It is used for academic purposes only
+            <figure id="csumb">
+                <img src="Cal-State-Monterey-Bay.jpg" width="100" height="50" alt="csumb logo" />
+            </figure>
+        </footer>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
